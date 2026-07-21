1fdab91d914f6afc6dd5b6896f02ccbb4fed8d0b
Arnoldrdh <arnoldrubben94@gmail.com>
2026-07-21 22:25:28 +0700

    feat: implement hellocontroller and routing example

---
+++ InventoryManagement.WebAPI/Controllers/HelloController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InventoryManagement.WebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HelloController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Hello World");
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            return Ok($"Hello, id = {id}");
+        }
+    }
+}
