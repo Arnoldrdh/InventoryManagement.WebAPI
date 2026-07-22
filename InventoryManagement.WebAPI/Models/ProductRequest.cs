0400aa3d7a53c10097f6f8d1fcd9425017326268
Arnoldrdh <arnoldrubben94@gmail.com>
2026-07-22 20:43:26 +0700

    feat: add request model validation

--- InventoryManagement.WebAPI/Models/ProductRequest.cs
+++ InventoryManagement.WebAPI/Models/ProductRequest.cs
@@ -1,9 +1,18 @@
-﻿namespace InventoryManagement.WebAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+
+
+namespace InventoryManagement.WebAPI.Models
 {
     public class ProductRequest
     {
+        [Required]
         public string Name { get; set; } = "";
+
+        [Range(1,int.MaxValue)]
         public int Price { get; set; }
+
+        [Range(1, int.MaxValue)]
         public int Stock { get; set; }
     }
 }
