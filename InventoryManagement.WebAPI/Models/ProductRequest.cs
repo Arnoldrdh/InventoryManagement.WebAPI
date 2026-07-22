bd302dd97e178002c0026e76996fa0e359cf9d9e
Arnoldrdh <arnoldrubben94@gmail.com>
2026-07-22 20:24:15 +0700

    feat: implement hello controler and request handling

---
+++ InventoryManagement.WebAPI/Models/ProductRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace InventoryManagement.WebAPI.Models
+{
+    public class ProductRequest
+    {
+        public string Name { get; set; } = "";
+        public int Price { get; set; }
+        public int Stock { get; set; }
+    }
+}
