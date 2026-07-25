a412d1d3e92c48a0f63e524fa957b96a3635998d
Arnoldrdh <arnoldrubben94@gmail.com>
2026-07-25 20:56:58 +0700

    Update: add Interface for product repository and service

---
+++ InventoryManagement.WebAPI/Models/ProductResponse.cs
@@ -0,0 +1,13 @@
+﻿namespace InventoryManagement.WebAPI.Models
+{
+    public class ProductResponse
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = "";
+
+        public int Price { get; set; }
+
+        public int Stock { get; set; }
+    }
+}
---
+++ InventoryManagement.WebAPI/Services/IProductService.cs
@@ -0,0 +1,17 @@
+﻿using InventoryManagement.WebAPI.Models;
+
+
+namespace InventoryManagement.WebAPI.Services
+{
+    public interface IProductService
+    {
+
+        List<ProductResponse> GetAllProduct();
+        ProductResponse? GetById(int id);
+        ProductResponse? Create(ProductRequest request);
+        ProductResponse? Update(int id, ProductRequest request);
+        bool Delete(int id);
+
+        
+    }
+}
